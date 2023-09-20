670e45af89d6b48cc5021e95153cf6aac363beed
Jay Cui <shilong@byu.edu>
2023-09-20 15:33:06 -0600

    Fixed linting errors

--- MyWidgetService/my_widget_service/my_widget_service_stack.py
+++ MyWidgetService/my_widget_service/my_widget_service_stack.py
@@ -1,13 +1,14 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+"""CDK stack for my_widget_service."""
+from aws_cdk import Stack  # Duration,; aws_sqs as sqs,
 from constructs import Construct
 
-class MyWidgetServiceStack(Stack):
 
+class MyWidgetServiceStack(Stack):
+    """CDK stack for my_widget_service."""
+
+    # pylint: disable-next=useless-parent-delegation
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        """Construct my_widget_service's CDK stack."""
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
--- MyWidgetService/tests/unit/test_my_widget_service_stack.py
+++ MyWidgetService/tests/unit/test_my_widget_service_stack.py
@@ -1,14 +1,18 @@
+"""Unit tests of MyWedgetService."""
 import aws_cdk as core
-import aws_cdk.assertions as assertions
+from aws_cdk import assertions
 
 from my_widget_service.my_widget_service_stack import MyWidgetServiceStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in my_widget_service/my_widget_service_stack.py
 def test_sqs_queue_created():
+    """Test to see if an SQS queue is created."""
     app = core.App()
     stack = MyWidgetServiceStack(app, "my-widget-service")
-    template = assertions.Template.from_stack(stack)
+    assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
